d8ff01b59e8597f691026c513e801d7476beab8c
FrostNT1 <ryanhugh014@gmail.com>
2025-12-11 15:17:23 -0600

    fix: Export ClusteringCache from environment module for docs
    - Add ClusteringCache to __init__.py exports
    - Fixes mkdocs build error: 'ClusteringCache could not be found'

--- rl_sc_cluster_utils/environment/__init__.py
+++ rl_sc_cluster_utils/environment/__init__.py
@@ -4,6 +4,7 @@ from rl_sc_cluster_utils.environment.actions import (
     ActionExecutor,
     convert_cluster_ids_to_numeric,
 )
+from rl_sc_cluster_utils.environment.cache import ClusteringCache
 from rl_sc_cluster_utils.environment.clustering_env import ClusteringEnv
 from rl_sc_cluster_utils.environment.rewards import RewardCalculator, RewardNormalizer
 from rl_sc_cluster_utils.environment.state import StateExtractor
@@ -14,5 +15,6 @@ __all__ = [
     "ActionExecutor",
     "RewardCalculator",
     "RewardNormalizer",
+    "ClusteringCache",
     "convert_cluster_ids_to_numeric",
 ]
